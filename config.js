f96232ec477419faad6b47321f4c885c0fab96c8
Quyen180103 <86756577+Quyen180103@users.noreply.github.com>
2021-07-01 10:57:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'mhtt à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao mh cứ đi nhầm đường
+    Lúc nào cubgx tretrau
+    Để mình tretrau mãi `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng mh đã tretrau ngay từ cái ngày sinh ra 😙',
+    desc: 'liệu mh tretrau đến hết đời ',
+    btnYes: 'đúng rồi  <33',
+    btnNo: 'Không,  :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao lại mình mh tretrau <3',
+    btnReply: 'Gửi cho t <3',
+    reply: 'tretrau thì tretrau thôi <33333333',
+    mess: 't biết mà 🥰. m không thoát tretrau đc đâu 😘😘',
+    messDesc: 'tretrau tiếp đi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
